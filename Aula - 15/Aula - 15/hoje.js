6ceace8cca02021ba59aa1f1c5fe2934517b4d47
gabriel-fporto <biellportoo@gmail.com>
2026-05-22 21:50:12 -0300

    Add files via upload

---
+++ Aula - 15/Aula - 15/hoje.js
@@ -0,0 +1,29 @@
+document.writeln('Olá, Mundo!!');
+
+// window.alert('Olá Mundo, seu lindo');
+
+document.getElementById('Titulo').innerHTML = "Olá Mundo JavaScript";
+let nomePrompt = '' //window.prompt("Olá aluno de JavaScript, qual o seu nome?")
+let resposta = false
+
+document.getElementById('nomeAluno').innerHTML = ("O nome do aluno JavaScript é: " + nomePrompt)     
+
+//window.confirm("Você gosta de JavaScript?")
+
+if (resposta) {
+    document.getElementById('nomeAluno').innerHTML = ("O aluno " + nomePrompt + " gosta de Javascript");
+}
+else {
+    document.getElementById('nomeAluno').innerHTML = ("O aluno " + nomePrompt + " NÃO gosta de Javascript");
+}
+
+function exibeNomeAluno() {
+    let inputAluno = document.getElementById('inputNomeAluno').value;
+
+    document.getElementById("resposta").innerHTML = "O nome digitado foi: " + inputAluno;
+}
+
+function apagarNomeAluno() {
+    document.getElementById('inputNomeAluno').value = "";
+    
+}
